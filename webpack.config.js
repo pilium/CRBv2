4a6822b7400454982ec01e12aba883e0276fee0c
pilium <igorpilium@gmail.com>
2019-05-16 16:02:04 +0300

    Add start template

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const webpack = require('webpack');
+const path = require('path');
+
+module.exports = {
+	output: {
+		filename: '[name].js',
+	},
+
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				use: {
+					loader: 'babel-loader',
+					query: {
+						presets: [
+							['@babel/preset-env', {
+								modules: false,
+								targets: {
+									node: 'current',
+								},
+								useBuiltIns: false,
+							}],
+						],
+					},
+				},
+			},
+		],
+	},
+	devtool: 'eval-source-map',
+	resolve: {
+		alias: {
+			'%modules%': path.resolve(__dirname, 'src/blocks/modules'),
+			'%components%': path.resolve(__dirname, 'src/blocks/components'),
+		},
+	},
+};
