75f7f0300cd6778f6213cd742143b5e03328902a
Igor Myasnikov <igorpilium@gmail.com>
2019-06-05 21:29:07 +0300

    Add map block

--- src/js/import/components.js
+++ src/js/import/components.js
@@ -2,3 +2,4 @@ import '../../blocks/components/modal/modal';
 import '../../blocks/components/input-text/input-text';
 import '../../blocks/components/select/select.js';
 import '../../blocks/components/checkbox/checkbox.js';
+import '../../blocks/components/map/map.js';
