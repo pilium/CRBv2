ee39075598fee6877d5f4670c209ebfe13a81814
pilium <igorpilium@gmail.com>
2019-06-05 10:35:38 +0300

    Add modal block

---
+++ src/blocks/components/modal/modal.js
@@ -0,0 +1,57 @@
+import MicroModal from 'micromodal';
+// eslint-disable-next-line no-unused-vars
+import SimpleBar from 'simplebar';
+export const types = document.querySelector('.feedback-types');
+
+export const content = document.querySelector('.modal__content');
+
+MicroModal.init({
+	onShow: (modal) => {
+		setTimeout(() => {
+			content.innerHTML = `
+				${modal.id}
+				<p>Dinamic contetn with many forms other types</p>
+				<p>Dinamic contetn with many forms other types</p>
+				<p>Dinamic contetn with many forms other types</p>
+				<p>Dinamic contetn with many forms other types</p>
+				<p>Dinamic contetn with many forms other types</p>
+				<p>Dinamic contetn with many forms other types</p>
+				<p>Dinamic contetn with many forms other types</p>
+				<p>Dinamic contetn with many forms other types</p>
+				<p>Dinamic contetn with many forms other types</p>
+				<p>Dinamic contetn with many forms other types</p>
+				<p>Dinamic contetn with many forms other types</p>
+				<p>Dinamic contetn with many forms other types</p>
+				<p>Dinamic contetn with many forms other types</p>
+				<p>Dinamic contetn with many forms other types</p>
+				<p>Dinamic contetn with many forms other types</p>
+				<p>Dinamic contetn with many forms other types</p>
+				<p>Dinamic contetn with many forms other types</p>
+				<p>Dinamic contetn with many forms other types</p>
+			`;
+		}, 2000000000000000);
+	},
+	onClose: () => {
+		const trigger = document.querySelector('.btn--write-us');
+
+		trigger.setAttribute('aria-expanded', 'false');
+	},
+	openTrigger: 'data-custom-open',
+	disableScroll: true,
+	disableFocus: false,
+	awaitCloseAnimation: true,
+});
+
+// Checkbox
+import {doctor} from '../../modules/checkbox/checkbox';
+const modalLine = document.querySelector('.modal__line');
+
+function doctorToggle() {
+	if (doctor.checked) {
+		modalLine.classList.add('active');
+	} else {
+		modalLine.classList.remove('active');
+	}
+}
+
+doctor.addEventListener('change', doctorToggle);
