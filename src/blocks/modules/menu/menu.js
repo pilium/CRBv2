02eff20fd34bf44784eb2e90a71c48dcd4cdf873
pilium <igorpilium@gmail.com>
2019-07-12 13:56:28 +0300

    Add dynamic height and position for mobile-menu

--- src/blocks/modules/menu/menu.js
+++ src/blocks/modules/menu/menu.js
@@ -1,11 +1,24 @@
 import {sandwich} from '../sandwich/sandwich';
 
+const headerHeight = document.querySelector('.header').clientHeight;
 const mainMenuLinks = document.querySelectorAll('.menu__link--has-submenu');
 const headerMenu = document.querySelector('.header-bottom__menu');
+
+function setHeightToMenu() {
+	if (headerMenu.classList.contains('active')) {
+		headerMenu.style.height = `calc(100% - ${headerHeight}px)`;
+		headerMenu.style.top = `${headerHeight}px`;
+	} else {
+		headerMenu.style.height = '0';
+		headerMenu.style.top = '200%';
+	}
+}
 
 function menuToggle() {
 	document.body.classList.toggle('fixed');
 	headerMenu.classList.toggle('active');
+
+	setHeightToMenu();
 }
 
 function mainMenuLinkHandler() {
