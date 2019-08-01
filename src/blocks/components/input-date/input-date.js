21e255914d6a0a7be3ab6cb0632ebe901f37fb3d
pilium <igorpilium@gmail.com>
2019-08-01 09:53:48 +0300

    Fix input-date block

--- src/blocks/components/input-date/input-date.js
+++ src/blocks/components/input-date/input-date.js
@@ -2,7 +2,7 @@ import flatpickr from 'flatpickr';
 import {Russian} from 'flatpickr/dist/l10n/ru';
 
 flatpickr('#testimonial-date', {
-	minDate: '2016-01',
+	minDate: '2017-01',
 	maxDate: 'today',
 	dateFormat: 'Y-m-d',
 	locale: Russian,
